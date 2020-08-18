ef6facbf6ea58cb6e8fc8b0513b9b70e11a4bd1d
Jorjis Hasan <63964149+jorjis212@users.noreply.github.com>
2020-08-18 23:24:41 +0600

    6th commit

---
+++ Take_screenshoot_using_python.py
@@ -0,0 +1,8 @@
+#-----taking screenshoot--☺
+import numpy
+import cv2
+import pyautogui
+img=pyautogui.screenshot()
+imgs=cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
+
+cv2.imwrite('image.png', imgs)
